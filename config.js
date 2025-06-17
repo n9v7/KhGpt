a955c085f9d49ffe4e14baea47c79a305bc4b072
n9v7 <22mbn34@gmail.com>
2025-06-17 04:06:41 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  API_URL: "http://localhost:8000/chat", // غيّرها حسب سيرفرك
+  OPENAI_API_KEY: "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",
+  TIMEOUT: 15000
+};
